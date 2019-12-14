1956218d42f34c4801b3011ddba2a63341c57f45
Вячеслав Рыжков <rugrisser@gmail.com>
2019-12-14 14:19:53 +0300

    CORS Fix 2

--- src/main.js
+++ src/main.js
@@ -3,12 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
-app.use(cors())
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
